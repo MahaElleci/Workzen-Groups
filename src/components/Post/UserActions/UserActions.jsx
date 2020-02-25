4c6c46a770ca43791f597d9e958b61810f0fc388
Mohamed Saber <Mohamed.Saber@itworx.com>
2020-02-25 16:11:54 +0200

    implement like toggling

--- src/components/Post/UserActions/UserActions.jsx
+++ src/components/Post/UserActions/UserActions.jsx
@@ -3,13 +3,25 @@ import Icon from "../../Shared/IcoMoon/IcoMoon";
 import './styles.scss';
 
 export class UserActions extends Component {
-  handleLike = () => {};
-  handleComment = () => {};
+  constructor() {
+    super();
+    this.state = {
+      isLikeClicked: false
+    }
+  }
+
+  handleLike = () => {
+    this.setState((state) => {
+      return { isLikeClicked: !state.isLikeClicked }
+    });
+  };
+  
+  handleComment = () => { };
   render() {
     return (
       <div className="actions-wrapper">
-        <button onClick={this.handleLike} className="actions-wrapper__btn">
-          <Icon icon="like" size="16" />
+        <button onClick={() => this.handleLike()} className="actions-wrapper__btn">
+          <Icon disableFill="true" color={this.state.isLikeClicked ? '#F97304' : '#555e71'} icon="like" size="16" />
           <span className="actions-wrapper__btn-text">Like</span>
         </button>
         <button onClick={this.handleComment} className="actions-wrapper__btn">
