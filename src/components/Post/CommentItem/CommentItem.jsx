10f9bb33234c1866d436ae4a2e2f977d616a012e
Maha Elleci <Maha.Elleci@itworx.com>
2020-02-25 15:58:18 +0200

    added comment layout

--- src/components/Post/CommentItem/CommentItem.jsx
+++ src/components/Post/CommentItem/CommentItem.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import './styles.scss';
+import React from "react";
+import "./styles.scss";
 
 function Comments({ data }) {
-    return (
-        <div className="comment">
-            <div className="comment__user-info">
-                <img alt="User" className="comment__user-image" src={data.userInfo.image} />
-                <div className="comment__user-text">
-                    <a href="#" className="username">{data.userInfo.name}</a>
-                    <small className="time-stamp">{data.timeCreated}</small>
-                </div>
-            </div>
-            <div className="comment__content">
-                {data.content}
-            </div>
+  return (
+    <div className="comment">
+      <img
+        alt="User"
+        className="comment__user-image"
+        src={data.userInfo.image}
+      />
+      <div className="comment__text">
+        <div className="comment__user-text">
+          <a href="#" className="username">
+            {data.userInfo.name}
+          </a>
+          <small className="time-stamp">{data.timeCreated}</small>
         </div>
-    )
+
+        <div className="comment__content">{data.content}</div>
+      </div>
+    </div>
+  );
 }
 
-export default Comments
-
+export default Comments;
